39cbca6bfbf10e6b7833864a8be2b01bb144abc4
Justin Bachtell <justin@justinbachtell.com>
2024-12-16 12:47:08 -0600

    ci(sentry): add sentry org and project config (#2)
    Added org, project, and authToken values for sentry
    
    BREAKING CHANGE: modified sentry config

--- next.config.ts
+++ next.config.ts
@@ -25,8 +25,9 @@ export default withSentryConfig(
     // For all available options, see:
     // https://github.com/getsentry/sentry-webpack-plugin#options
     // FIXME: Add your Sentry organization and project names
-    org: 'nextjs-boilerplate-org',
-    project: 'nextjs-boilerplate',
+    org: 'justin-bachtell',
+    project: 'smartvest-bot',
+    authToken: process.env.SENTRY_AUTH_TOKEN,
 
     // Only print logs for uploading source maps in CI
     silent: !process.env.CI,
